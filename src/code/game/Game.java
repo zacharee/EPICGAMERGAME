9a7c8cb2e3025ae02aff48ccec27e6940c1b5f55
Evin <evin.kierans100@gmail.com>
2019-07-19 11:46:19 +0100

    Made window

--- src/code/game/Game.java
+++ src/code/game/Game.java
@@ -1,5 +1,33 @@
-public class Game {
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable{
+
+    public static final int WIDTH = 800, HEIGHT = 600;
+    private Thread thread;
+    private boolean running = false;
+
+    public Game() {
+
+        new Window(WIDTH, HEIGHT, "BudgetScrolls", this);
+
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    public void run() {
+        try {
+            thread.join();
+            running = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.print("hello world");
+        new Game();
     }
 }
