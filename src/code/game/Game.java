fbb9878a4817fea99c6083fddc3fc052cbf606ca
Evin <evin.kierans100@gmail.com>
2019-07-19 11:53:47 +0100

    Added game loop and FPS counter

--- src/code/game/Game.java
+++ src/code/game/Game.java
@@ -18,13 +18,50 @@ public class Game extends Canvas implements Runnable{
         running = true;
     }
 
-    public void run() {
+    public void stop() {
         try {
             thread.join();
             running = false;
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void run() {
+        long lastTime = System.nanoTime();
+        double amountofTicks = 60.0;
+        double ns = 1000000000 / amountofTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1) {
+                tick();
+                delta--;
+            }
+            if(running) {
+                render();
+            }
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    public void tick() {
+
+    }
+
+    public void render() {
+
     }
 
     public static void main(String[] args) {
