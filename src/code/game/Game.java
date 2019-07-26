98b5ffdb4b68a8c7444075ac1d3e1cf944d228b1
Real-EviN <evin.kierans100@gmail.com>
2019-07-26 01:31:17 +0100

    Adjusted minion height to account for lightning attack

--- src/code/game/Game.java
+++ src/code/game/Game.java
@@ -31,7 +31,7 @@ public class Game extends Canvas implements Runnable{
         handler.addObject(new Ground(0, 460, ID.Ground));
         handler.addObject(new Platform(690, 350, ID.Platform, "assets/GrassPlatform.png"));
         handler.addObject(player);
-        handler.addObject(new WeakMinion(WIDTH-50, 452, ID.WeakMinion));
+        handler.addObject(new WeakMinion(WIDTH-50, 432, ID.WeakMinion));
         handler.addObject(new DoubleJumpPowerup(WIDTH/2, 400, ID.DoubleJumpPowerup));
         for(int i = 600; i < 5601; i = i + 500) {
             handler.addObject(new HealthPowerUp(i, 400, ID.HealthPowerup));
