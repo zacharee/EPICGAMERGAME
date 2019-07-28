f1b0c777d1b2cd8b99a27f390cb9ae2290e730c0
Real-EviN <evin.kierans100@gmail.com>
2019-07-28 16:35:32 +0100

    Fixed TODO

--- src/code/game/Game.java
+++ src/code/game/Game.java
@@ -4,7 +4,7 @@ import java.awt.image.BufferStrategy;
 //TODO
 // fix it so that you can't go left further than where you spawned
 // Fix movement - double jump inconsistencies and velocity changes
-// Make meilueghghg attacks be 1 quick swipe instead of a constant one
+// Make meilueghghg sword attacks be 1 quick swipe instead of a constant one
 
 public class Game extends Canvas implements Runnable{
 
